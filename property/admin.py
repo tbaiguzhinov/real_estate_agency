d2aee6c80256c72575e2b979806da9fcfbdbad0c
tbaiguzhinov <tim.baiguzhinov@gmail.com>
2022-06-10 12:44:01 +0600

    add db model for likes

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat, Likes_And_Complaints
 
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ("town", "address", "owner",)
@@ -9,4 +9,8 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ("new_building",)
     list_filter = ("new_building","rooms_number", "has_balcony", "active",)
 
+class Likes_And_ComplaintsAdmin(admin.ModelAdmin):
+    raw_id_fields = ("user", "flat",)
+
 admin.site.register(Flat, FlatAdmin)
+admin.site.register(Likes_And_Complaints, Likes_And_ComplaintsAdmin)
--- property/models.py
+++ property/models.py
@@ -1,5 +1,7 @@
+from tabnanny import verbose
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Flat(models.Model):
@@ -50,3 +52,11 @@ class Flat(models.Model):
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
+class Likes_And_Complaints(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name="Кто жаловался")
+    flat = models.ForeignKey(Flat, on_delete=models.CASCADE, verbose_name="Квартира, на которую пожаловались")
+    complaint_text = models.TextField("Текст жалобы")
+
+    class Meta:
+        verbose_name_plural = "Likes_And_Complaints"
